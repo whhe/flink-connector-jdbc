dd35b05b9f26fa6430cc6bca1b433d1303d3a4da
He Wang <wanghechn@qq.com>
2024-07-03 18:51:28 +0800

    remove password

--- flink-connector-jdbc/src/test/java/org/apache/flink/connector/jdbc/testutils/databases/oceanbase/OceanBaseDatabase.java
+++ flink-connector-jdbc/src/test/java/org/apache/flink/connector/jdbc/testutils/databases/oceanbase/OceanBaseDatabase.java
@@ -27,8 +27,9 @@ import org.slf4j.LoggerFactory;
 import org.testcontainers.containers.Network;
 import org.testcontainers.containers.output.Slf4jLogConsumer;
 import org.testcontainers.containers.wait.strategy.Wait;
-import org.testcontainers.images.builder.Transferable;
 
+import java.sql.Connection;
+import java.sql.Statement;
 import java.time.Duration;
 import java.time.Instant;
 import java.time.ZoneId;
@@ -48,13 +49,10 @@ public class OceanBaseDatabase extends DatabaseExtension implements OceanBaseIma
             new OceanBaseContainer(OCEANBASE_CE_4)
                     .withNetwork(NETWORK)
                     .withEnv("MODE", "slim")
-                    .withPassword("123456")
+                    .withEnv("OB_DATAFILE_SIZE", "2G")
+                    .withEnv("OB_LOG_DISK_SIZE", "4G")
                     .withUrlParam("useSSL", "false")
                     .withUrlParam("serverTimezone", ZONE_OFFSET)
-                    .withCopyToContainer(
-                            Transferable.of(
-                                    String.format("SET GLOBAL time_zone = '%s';", ZONE_OFFSET)),
-                            "/root/boot/init.d/init.sql")
                     .waitingFor(Wait.forLogMessage(".*boot success!.*", 1))
                     .withStartupTimeout(Duration.ofMinutes(2))
                     .withLogConsumer(new Slf4jLogConsumer(LOG));
@@ -80,6 +78,10 @@ public class OceanBaseDatabase extends DatabaseExtension implements OceanBaseIma
     @Override
     protected DatabaseMetadata startDatabase() throws Exception {
         CONTAINER.start();
+        try (Connection connection = getMetadata().getConnection();
+                Statement statement = connection.createStatement()) {
+            statement.execute(String.format("SET GLOBAL time_zone = '%s'", ZONE_OFFSET));
+        }
         return getMetadata();
     }
 
